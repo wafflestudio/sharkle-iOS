d3fd64ce3f64bfb89e448cc5e5fa6b971b747321
KIMWOOSUNG <40kimwoosung@gmail.co>
2022-02-27 11:39:11 +0900

    Initial setup

---
+++ sharkle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sharkle
+//
+//  Created by 김우성 on 2022/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sharkle/sharkleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharkleApp.swift
+//  sharkle
+//
+//  Created by 김우성 on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct sharkleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
