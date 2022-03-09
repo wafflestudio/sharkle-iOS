a0dce6d52a793ade4f6692e7310ea1db9dd974ba
KIMWOOSUNG <40kimwoosung@gmail.co>
2022-03-09 19:29:13 +0900

    Create ClubItem

---
+++ sharkle/ClubItem.swift
@@ -0,0 +1,37 @@
+//
+//  ClubItem.swift
+//  sharkle
+//
+//  Created by 김우성 on 2022/03/09.
+//
+
+import SwiftUI
+
+struct ClubItem: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Image(systemName: "")
+                .frame(width: 84, height: 84, alignment: .center)
+                .background(.gray)
+                .cornerRadius(15)
+            
+            Text("동아리 이름 1")
+                .frame(width: 84)
+                .font(.system(size: 14, weight: .medium))
+                .lineLimit(1)
+            
+            Text("#태그")
+                .frame(width: 32, height: 14, alignment: .center)
+                .background(Color("TagTextBackgroundColor"))
+                .cornerRadius(7)
+                .font(.system(size: 10, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct ClubItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ClubItem()
+    }
+}
