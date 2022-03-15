5c6ec8dc0a677cbac85b673342aacfeebf4e8609
KIMWOOSUNG <40kimwoosung@gmail.co>
2022-03-15 17:19:41 +0900

    ContentView code delete

--- Sharkle/ContentView.swift
+++ Sharkle/ContentView.swift
@@ -6,27 +6,11 @@
 //
 
 import SwiftUI
-import Combine
-import Moya
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-    }
-    
-    let provide = MoyaProvider<AuthenitcationAPI>()
-    var cancellable: AnyCancellable
-    
-    init() {
-        cancellable = provide.requestPublisher(.signup(email: "40kimwoosung@gmail.com", user_id: "asdfjklgh1234", password: "password", username: "KIM"))
-            .sink(receiveCompletion: { completion in
-                guard case let .failure(error) = completion else { return }
-
-                print(error)
-            }, receiveValue: { response in
-                print(response)
-            })
     }
 }
 
